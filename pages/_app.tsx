a316c76204234fe7f870af2f65b9ccbc66d5e729
DUSTINRYOO <hyeony77@gmail.com>
2023-02-22 23:23:43 -0500

    Apollo Setting
    Chrome needs to be used to see the connection with server in Apollo dev tool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import client from "./apollo";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-white-400 mx-auto  w-full">
-      <Component {...pageProps} />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="bg-white-400 mx-auto  w-full">
+        <Component {...pageProps} />
+      </div>
+    </ApolloProvider>
   );
 }
