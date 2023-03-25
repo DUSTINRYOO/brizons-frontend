b12622a62ff848a5d40003e0adf5b402e0e1c293
DUSTINRYOO <hyeony77@gmail.com>
2023-03-25 16:55:25 -0400

    children briz page pinned updated

--- pages/briz/[username]/index.tsx
+++ pages/briz/[username]/index.tsx
@@ -539,6 +539,7 @@ const Briz: NextPage = () => {
                   <motion.div
                     key={i}
                     layout
+                    initial={{ opacity: 0 }}
                     exit={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     className="relative"
@@ -549,7 +550,7 @@ const Briz: NextPage = () => {
                       href={`/briz/${brizUserName}/${briz.id}`}
                     >
                       <motion.div
-                        className="relative aspect-square h-[10vw] overflow-hidden rounded-full border-4 border-gray-300 bg-white  shadow-lg"
+                        className="relative aspect-square h-[10vw] overflow-hidden rounded-full border-4 border-gray-100 bg-white  shadow-lg"
                         style={{
                           height: `clamp(1px,10vw,8rem)`,
                           margin: `clamp(1px,1vw,0.8rem)`,
@@ -558,7 +559,7 @@ const Briz: NextPage = () => {
                         variants={{
                           hoverBox: {
                             scale: 1.03,
-                            borderColor: "rgb(156 163 175)",
+                            borderColor: "rgb(254 202 202)",
                           },
                         }}
                       >
@@ -1047,9 +1048,6 @@ const Briz: NextPage = () => {
                           <motion.div
                             className=" relative my-4 aspect-square w-full overflow-hidden rounded-xl bg-gray-50 shadow-lg"
                             whileTap={{ scale: 1.05 }}
-                            onClick={() => {
-                              console.log("clicked");
-                            }}
                           >
                             <Image
                               priority
@@ -1071,7 +1069,7 @@ const Briz: NextPage = () => {
                             onClickPinned(briz.id, !briz.pinned);
                           }}
                           className={cls(
-                            " absolute right-2 top-2 flex aspect-square cursor-pointer items-center justify-center rounded-2xl bg-red-100 p-1 shadow-xl transition-all hover:bg-red-300 active:scale-105"
+                            " absolute right-2 top-2 flex aspect-square cursor-pointer items-center justify-center rounded-2xl p-1 shadow-xl transition-all hover:scale-105"
                           )}
                         >
                           <svg
@@ -1082,7 +1080,7 @@ const Briz: NextPage = () => {
                           >
                             <path
                               d="M32 32C32 14.3 46.3 0 64 0H320c17.7 0 32 14.3 32 32s-14.3 32-32 32H290.5l11.4 148.2c36.7 19.9 65.7 53.2 79.5 94.7l1 3c3.3 9.8 1.6 20.5-4.4 28.8s-15.7 13.3-26 13.3H32c-10.3 0-19.9-4.9-26-13.3s-7.7-19.1-4.4-28.8l1-3c13.8-41.5 42.8-74.8 79.5-94.7L93.5 64H64C46.3 64 32 49.7 32 32zM160 384h64v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384z"
-                              fill={briz.pinned ? "black" : "white"}
+                              fill={briz.pinned ? "black" : "rgb(229 231 235)"}
                             />
                           </svg>
                         </button>
