b5ed94817d9288b66b91d972807e1f580a7223c2
DUSTINRYOO <hyeony77@gmail.com>
2023-04-19 22:33:48 -0400

    Tailwind Purge added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./src/**/*.tsx"],
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
